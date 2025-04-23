2d5404c1ee18cc744b5fc5d8d4fc4d1a829d931c
vs-machado <vinicius.s.machado@protonmail.com>
2025-04-23 09:29:20 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# church_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/church_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.church_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
