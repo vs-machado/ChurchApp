92f725c70c7b73c14145df9be909d3c302021c2b
vs-machado <vinicius.s.machado@protonmail.com>
2025-06-10 16:07:11 -0300

    CommentItem criado

---
+++ lib/feature/posts/presentation/components/comment_item.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+// class CommentItem extends StatelessWidget {
+//   const CommentItem({super.key, required this.comment});
+//
+//   final CommentUi comment;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: Row(
+//         children: [
+//           CircleAvatar(
+//             backgroundImage:
+//                 comment.userAvatarUrl != null
+//                     ? NetworkImage(comment.userAvatarUrl!)
+//                     : const AssetImage(
+//                           'lib/core/assets/images/profile_image.png',
+//                         )
+//                         as ImageProvider,
+//             radius: 20.0,
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
--- lib/feature/posts/presentation/pages/post_details_page.dart
+++ lib/feature/posts/presentation/pages/post_details_page.dart
@@ -1,3 +1,4 @@
+import 'package:church_app/feature/posts/presentation/components/comment_item.dart';
 import 'package:church_app/feature/posts/presentation/components/post_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -26,7 +27,15 @@ class _PostDetailsPageState extends ConsumerState<PostDetailsPage> {
         child: Column(
           children: [
             Expanded(
-              child: SingleChildScrollView(child: PostItem(post: widget.post)),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    PostItem(post: widget.post),
+                    const SizedBox(height: 8.0),
+                    // CommentItem(),
+                  ],
+                ),
+              ),
             ),
             Container(
               padding: const EdgeInsets.all(8.0),
