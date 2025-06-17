40208cf976bfa4b605c6d7bb1159e9d7a59d2bfc
vs-machado <vinicius.s.machado@protonmail.com>
2025-06-17 15:27:53 -0300

    Caching de imagens implementado nos posts

--- lib/feature/posts/presentation/components/post_item.dart
+++ lib/feature/posts/presentation/components/post_item.dart
@@ -1,3 +1,5 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:church_app/core/data/cache_manager/custom_cache_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:church_app/feature/posts/presentation/models/post_ui.dart';
 
@@ -35,243 +37,237 @@ class _PostItemState extends State<PostItem> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 10.0),
-          child: Row(
-            children: [
-              CircleAvatar(
-                backgroundImage:
-                    widget.post.user.avatarUrl != null
-                        ? NetworkImage(widget.post.user.avatarUrl!)
-                        : const AssetImage(
-                              'lib/core/assets/images/profile_image.png',
+    return Card(
+      color: Colors.transparent,
+      elevation: 0,
+      margin: const EdgeInsets.symmetric(vertical: 0),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 10.0),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage:
+                        widget.post.user.avatarUrl != null
+                            ? CachedNetworkImageProvider(
+                              widget.post.user.avatarUrl!,
                             )
-                            as ImageProvider,
-                radius: 20.0,
-              ),
-              const SizedBox(width: 10.0),
-              Padding(
-                padding: const EdgeInsets.only(top: 2.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    InkWell(
-                      onTap: () {},
-                      child: Text(
-                        style: const TextStyle(
-                          fontWeight: FontWeight.w800,
-                          fontSize: 16.0,
+                            : const AssetImage(
+                                  'lib/core/assets/images/profile_image.png',
+                                )
+                                as ImageProvider,
+                    radius: 20.0,
+                  ),
+                  const SizedBox(width: 10.0),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 2.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        InkWell(
+                          onTap: () {},
+                          child: Text(
+                            widget.post.user.name,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w800,
+                              fontSize: 16.0,
+                            ),
+                          ),
                         ),
-                        widget.post.user.name,
-                      ),
+                        Text(
+                          widget.post.timeSincePosted,
+                          style: const TextStyle(fontSize: 12.0),
+                        ),
+                      ],
                     ),
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 10.0),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 14.0),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(
+                    context,
+                    '/postDetails',
+                    arguments: widget.post,
+                  );
+                },
+                child: LayoutBuilder(
+                  builder: (context, constraints) {
+                    final text = widget.post.text;
+                    final TextSpan textSpan = TextSpan(
+                      text: text,
+                      style: const TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.black,
+                      ),
+                    );
+                    final TextPainter textPainter = TextPainter(
+                      text: textSpan,
+                      maxLines: 6,
+                      textDirection: TextDirection.ltr,
+                    )..layout(maxWidth: constraints.maxWidth);
+
+                    final bool isTextOverflowing =
+                        textPainter.didExceedMaxLines;
+
+                    if (isTextOverflowing) {
+                      final charactersPerLine =
+                          text.length /
+                          (textPainter.height /
+                              textPainter.preferredLineHeight);
+                      final visibleTextLength =
+                          (charactersPerLine * 4.8).toInt();
+                      final visibleText = text.substring(
+                        0,
+                        visibleTextLength > text.length
+                            ? text.length
+                            : visibleTextLength,
+                      );
+
+                      return RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                              text: '$visibleText... ',
+                              style: const TextStyle(
+                                fontSize: 14.0,
+                                color: Colors.black,
+                              ),
+                            ),
+                            const TextSpan(
+                              text: 'Ver mais',
+                              style: TextStyle(
+                                fontSize: 14.0,
+                                color: Colors.purple,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        textAlign: TextAlign.start,
+                      );
+                    } else {
+                      return Text(
+                        text,
+                        style: const TextStyle(color: Colors.black),
+                        textAlign: TextAlign.start,
+                      );
+                    }
+                  },
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 10.0),
+
+            if (widget.post.videoUrl != null) ...[
+              const Text('Video Player Placeholder'),
+              const SizedBox(height: 10.0),
+            ] else if (widget.post.photoUrl != null) ...[
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12.0),
+                  child: CachedNetworkImage(
+                    cacheManager: CustomCacheManager.instance,
+                    imageUrl: widget.post.photoUrl!,
+                    placeholder:
+                        (context, url) =>
+                            const Center(child: CircularProgressIndicator()),
+                    errorWidget:
+                        (context, url, error) => const Icon(Icons.error),
+                    width: double.infinity,
+                    height: 250,
+                    // ✅ Fixed height to remove blank space issues
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10.0),
+            ],
+
+            if (widget.post.likesCount > 0) ...{
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
                     Text(
-                      style: const TextStyle(fontSize: 12.0),
-                      widget.post.timeSincePosted,
+                      S.of(context).likesCount(widget.post.likesCount),
+                      style: const TextStyle(fontSize: 14.0),
+                    ),
+                    const SizedBox(width: 5.0),
+                    const Icon(
+                      Icons.thumb_up,
+                      color: Colors.deepPurple,
+                      size: 16.0,
                     ),
                   ],
                 ),
               ),
-            ],
-          ),
-        ),
-        const SizedBox(height: 10.0),
-
-        // Conteúdo do post em texto
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 14.0),
-          child: GestureDetector(
-            onTap: () {
-              Navigator.pushNamed(
-                context,
-                '/postDetails',
-                arguments: widget.post,
-              );
             },
-            child: LayoutBuilder(
-              builder: (context, constraints) {
-                final text = widget.post.text;
-                final TextSpan textSpan = TextSpan(
-                  text: text,
-                  style: const TextStyle(fontSize: 14.0, color: Colors.black),
-                );
-                final TextPainter textPainter = TextPainter(
-                  text: textSpan,
-                  maxLines: 6,
-                  textDirection: TextDirection.ltr,
-                )..layout(maxWidth: constraints.maxWidth);
 
-                final bool isTextOverflowing = textPainter.didExceedMaxLines;
-
-                if (isTextOverflowing) {
-                  final charactersPerLine =
-                      text.length /
-                      (textPainter.height / textPainter.preferredLineHeight);
-                  final visibleTextLength = (charactersPerLine * 4.8).toInt();
-                  final visibleText = text.substring(
-                    0,
-                    visibleTextLength > text.length
-                        ? text.length
-                        : visibleTextLength,
-                  );
-
-                  return RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                          text: '$visibleText... ',
-                          style: const TextStyle(
-                            fontSize: 14.0,
-                            color: Colors.black,
-                          ),
-                        ),
-                        const TextSpan(
-                          text: 'Ver mais',
-                          style: TextStyle(
-                            fontSize: 14.0,
-                            color: Colors.purple,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                    textAlign: TextAlign.start,
-                  );
-                } else {
-                  return Text(
-                    text,
-                    style: const TextStyle(color: Colors.black),
-                    textAlign: TextAlign.start,
-                  );
-                }
-              },
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Divider(height: 15.0),
             ),
-          ),
-        ),
-        const SizedBox(height: 10.0),
 
-        // Exibe um vídeo ou imagem caso o usuário faça upload.
-        // A implementação atual será de apenas exibir uma imagem ou um vídeo.
-        // Caso por alguma falha do código o usuário faça upload de uma foto e um vídeo
-        // apenas o vídeo será exibido.
-        if (widget.post.videoUrl != null) ...[
-          const Text('Video Player Placeholder'),
-          const SizedBox(height: 10.0),
-        ] else if (widget.post.photoUrl != null) ...[
-          GestureDetector(
-            onTap: () {
-              Navigator.pushNamed(
-                context,
-                '/postDetails',
-                arguments: widget.post,
-              );
-            },
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12.0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(12.0),
-                child: Image.network(
-                  widget.post.photoUrl!,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(height: 10.0),
-        ],
-
-        // Exibe a contagem de curtidas caso alguém curta a postagem
-        if (widget.post.likesCount > 0) ...{
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                // Likes count
-                Text(
-                  S.of(context).likesCount(widget.post.likesCount),
-                  style: TextStyle(fontSize: 14.0),
+                TextButton.icon(
+                  onPressed: () {
+                    setState(() {
+                      isLiked = !isLiked;
+                    });
+
+                    if (isLiked) {
+                      _likeController.forward();
+                    } else {
+                      _likeController.reverse();
+                    }
+                  },
+                  icon: AnimatedBuilder(
+                    animation: _likeController,
+                    builder: (context, child) {
+                      return Transform.scale(
+                        scale: 1.0 + (_likeController.value * 0.4),
+                        child: Icon(
+                          isLiked ? Icons.thumb_up : Icons.thumb_up_outlined,
+                          color: isLiked ? Colors.deepPurple : null,
+                        ),
+                      );
+                    },
+                  ),
+                  label: Text(
+                    isLiked ? 'Descurtir' : 'Curtir',
+                    style: TextStyle(color: isLiked ? Colors.deepPurple : null),
+                  ),
                 ),
-                const SizedBox(width: 5.0),
-                const Icon(
-                  Icons.thumb_up,
-                  color: Colors.deepPurple,
-                  size: 16.0,
+                TextButton.icon(
+                  onPressed: () => widget.onCommentPressed?.call(),
+                  icon: const Icon(Icons.chat_bubble_outline),
+                  label: const Text('Comentar'),
                 ),
-                // TODO: implement a comments counter
+                TextButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.share_outlined),
+                  label: const Text('Compartilhar'),
+                ),
               ],
-            ),
-          ),
-        },
-
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: Divider(height: 15.0),
-        ),
-
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            _buildAnimatedButton(
-              onPressed: () {
-                setState(() {
-                  isLiked = !isLiked;
-                });
-                // TODO: check if it's possible to update the like counter to increment the user like
-                // TODO: needs to update both the string and the database
-                if (isLiked) {
-                  _likeController.forward();
-                } else {
-                  _likeController.reverse();
-                }
-              },
-              icon: AnimatedBuilder(
-                animation: _likeController,
-                builder: (context, child) {
-                  return Transform.scale(
-                    scale: 1.0 + (_likeController.value * 0.4),
-                    child: Icon(
-                      isLiked ? Icons.thumb_up : Icons.thumb_up_outlined,
-                      color: isLiked ? Colors.deepPurple : null,
-                    ),
-                  );
-                },
-              ),
-              label: isLiked ? 'Descurtir' : 'Curtir',
-              isActive: isLiked,
-            ),
-            TextButton.icon(
-              onPressed: () => widget.onCommentPressed?.call(),
-              icon: const Icon(Icons.chat_bubble_outline),
-              label: const Text('Comentar'),
-            ),
-            TextButton.icon(
-              onPressed: () {},
-              icon: const Icon(Icons.share_outlined),
-              label: const Text('Compartilhar'),
             ),
           ],
         ),
-      ],
-    );
-  }
-
-  Widget _buildAnimatedButton({
-    required VoidCallback onPressed,
-    required Widget icon,
-    required String label,
-    required bool isActive,
-  }) {
-    return TextButton.icon(
-      onPressed: onPressed,
-      icon: icon,
-      label: Text(
-        label,
-        style: TextStyle(color: isActive ? Colors.deepPurple : null),
       ),
     );
   }
